8b4f578a8d72ad74e588066b182e10a7defe8e27
Sonam-22 <sonampriya7070@gmail.com>
2022-06-26 20:15:58 +0200

    adds start screen, chat screen and navigation between them

---
+++ components/Chat.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Chat(props) {
+  let { name, color } = props.route.params;
+  // Set the screen title to the user name entered in the start screen
+  useEffect(() => {
+    props.navigation.setOptions({ title: name });
+  }, []);
+  return (
+    <View style={[{ backgroundColor: color }, styles.container]}>
+      <Text style={styles.text}>Hello Chat!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  text: {
+    color: "#ffffff",
+  },
+});
